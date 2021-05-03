f4f6855ca0d4f52adfee4d47dadcd80271b75b41
Maciej Rudnik <maciej.rudnik2@gmail.com>
2021-05-03 23:48:29 +0200

    create project

---
+++ src/main/java/com/appsdeveloper/app/ws/SpringMicroservicesCloudApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloper.app.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroservicesCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroservicesCloudApplication.class, args);
+	}
+
+}
